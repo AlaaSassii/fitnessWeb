d211eb9e0d36e084532c49b94b6731bdec6e5ff7
alaasassii <alaasassijs@gmail.com>
2023-10-10 21:03:33 +0100

    add new things

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./programs.css";
+import { programsData } from "../../data/programsData";
 const Programs = () => {
   return (
     <div className='Programs' id='programs'>
       <div className='programs-header'>
-        <span>Explore our</span>
+        <span className='stroke-text'>Explore our</span>
         <span>Programs</span>
-        <span>to shape you</span>
+        <span className='stroke-text'>to shape you</span>
+      </div>
+      <div className='programs-categories'>
+        {programsData.map((program, index) => (
+          <div className='category' key={`program__${index}`}>
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
