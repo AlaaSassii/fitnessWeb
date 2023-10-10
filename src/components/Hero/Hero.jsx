8629cf5343bd55766698cb7d043a6a6b884b631f
alaasassii <alaasassijs@gmail.com>
2023-10-10 19:18:28 +0100

    add styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,12 @@ const Hero = () => {
     <div className='hero'>
       <div className='left-h'>
         <Header />
+        <div className='the-best-ad'>
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
       </div>
-      <div className='right-h'></div>
+      <div className='right-h'>right</div>
     </div>
   );
 };
