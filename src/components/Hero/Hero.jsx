cc29b06a74b11eb92cffd6098cb93aaea565293b
alaasassii <alaasassijs@gmail.com>
2023-10-10 19:43:18 +0100

    add data for the servive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,10 +20,32 @@ const Hero = () => {
           <div>
             <span>Ideal body</span>
           </div>
-          <div className='span'>
-            in here we will help you to shape and build your ideal body and live
-            up your life to fullest
+          <div>
+            <span>
+              in here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
           </div>
+        </div>
+
+        <div className='figures'>
+          <div>
+            <span>+ 140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+ 978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        <div className='hero-buttons'>
+          <button className='btn'>Get Started</button>
+          <button className='btn'>Learn More</button>
         </div>
       </div>
 
