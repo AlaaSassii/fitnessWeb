aa3c6673c4d9b3845ebd37a5fe92eac58e9fb361
alaasassii <alaasassijs@gmail.com>
2023-10-11 17:05:23 +0100

    add new things

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./Plans.css";
+import { planData } from "../../data/plansData";
 const Plans = () => {
-  return <div></div>;
+  return (
+    <div className='plans-container'>
+      <div className='program-header' style={{ gap: "2rem" }}>
+        <span className='stroke-text'>READY TO START</span>
+        <span>YOUR JOURNEY</span>
+        <span className='stroke-text'>NOW WITHUS</span>
+      </div>
+    </div>
+  );
 };
 
 export default Plans;
