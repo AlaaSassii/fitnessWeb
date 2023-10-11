1ccc64e866f36f3442c8dac1f3d0b4c4cac1e9bb
alaasassii <alaasassijs@gmail.com>
2023-10-11 16:49:35 +0100

    complete Reasons

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Reasons from "./components/Reasons/Reasons";
+import Plans from "./components/Plans/Plans";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </div>
   );
 };
