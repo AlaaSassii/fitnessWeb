8c2d315ff27dca750ffdf99dd191bd68f4c17ab0
alaasassii <alaasassijs@gmail.com>
2023-10-15 07:38:15 +0100

    add responsivness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
+
 const Header = () => {
+  const isMobile = window.innerWidth <= 768;
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
     <div className='header'>
-      <img src={Logo} alt='' className='logo' />
-      <ul className='header-menu'>
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plants</li>
-        <li>Testimonials</li>
-      </ul>
+      <img src={Logo} alt='Logo' className='logo' />
+      {!menuOpened && isMobile ? (
+        <div>
+          <img src={Bars} alt='Bars' />
+        </div>
+      ) : (
+        <ul className='header-menu'>
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plants</li>
+          <li>Testimonials</li>
+        </ul>
+      )}
     </div>
   );
 };
